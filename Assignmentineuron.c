9b62a499028cd8d7b8fb56b75b81e82b7b3fbce2
SandeepKumarNishad <105138121+SandeepKumarNishad@users.noreply.github.com>
2022-07-14 15:37:50 +0530

    Add files via upload

---
+++ Assignmentineuron.c
@@ -0,0 +1,88 @@
+/*1. Write a program to print Hello Students on the screen
+#include<stdio.h>
+int main()
+{
+    printf("Hello Students");
+    return 0;
+}
+//2. Write a program to print Hello in the first line and Students in the second line
+#include<stdio.h>
+int main()
+{
+    printf("Hello\nStudents");
+    return 0;
+}
+3. Write a program to print �MySirG� on the screen. (Remember to print in double quotes)
+#include<stdio.h>
+int main()
+{
+    printf("\"MySirG\"");
+    return 0;
+}
+4. WAP to find the area of the circle. Take radius of circle from user as input and print the
+result in below given format.
+#include<stdio.h>
+int main()
+{
+  float A,R;
+  printf("Enter the radius\n");
+  scanf("%f",&R);
+  A=(3.14*R*R);
+  printf("Area of circle %f having the radius %f",A,R);
+  return 0;
+}
+5. WAP to calculate the length of String using printf function
+#include<stdio.h>
+int main()
+{
+    int x=printf("pushkar");
+    printf("%d",x);
+    return 0;
+}
+6. WAP to print the name of the user in double quotes
+#include<stdio.h>
+int main()
+{
+    printf("\"Hello,AmitKumar\"");
+    return 0;
+}
+7. WAP to print �%d� on the screen
+#include<stdio.h>
+int main()
+{
+    printf("%%d");
+    return 0;
+
+}
+8. WAP to print �\n� on the screen.
+#include<stdio.h>
+int main()
+{
+    printf("\\n");
+    return 0;
+}
+9. WAP to print �\\� on the screen.
+#include<stdio.h>
+int main()
+{
+    printf("\\\\");
+    return 0;
+}
+10. WAP to take date as an input in below given format and convert the date format and
+display the result as given below.
+User Input date format � �DD/MM/YYYY� (27/11/2022)
+Output format �
+�Day � DD , Month � MM , Year � YYYY� (Day � 27 ,Month � 07 , Year � 2022)
+#include<stdio.h>
+int main()
+{
+
+}
+12. Find output of below code:
+#include<stdio.h>
+int main()
+{
+    int x=printf("ineuron");
+    printf("%d",x);
+    return 0;
+}*/
